964ff113476f4e28d8847aa4e3520376cf2a9e75
konstds <konstds@konstdss-MacBook-Pro.local>
2020-12-15 23:56:47 +0300

    advent 10

--- Advent of code/advent10.py
+++ Advent of code/advent10.py
@@ -36,11 +36,37 @@ def find_jolt(num_list):
     return jolt1, jolt3
 
 
-def find_ways(num_list):
-    
+def find_ways(min, num_list, max, check_list):
+    counter = 0
 
-with open('input10_0.txt', 'r') as file_in:
-        num_list = read_file(file_in)
+    for i in range(min, max):
+        print(f'i = {i}, num_list[i] = {num_list[i]}, num_list[max-1] = {num_list[max-1]}, check_list = {check_list}')
+        if num_list[i] == num_list[max - 1]:
+
+            counter += 1
+            break
+        if num_list[i] + 1 in num_list:
+            check_list.append(num_list[i] + 1)
+            print(f'i = {i}, num_list[i] + 1 = {num_list[i] + 1}, check_list = {check_list}')
+            start = num_list.index(num_list[i] + 1)
+            counter += find_ways(start, num_list, max, check_list)
+        elif num_list[i] + 2 in num_list:
+            check_list.append(num_list[i] + 2)
+            print(f'i = {i}, num_list[i] + 2 = {num_list[i] + 2}, check_list = {check_list}')
+            start = num_list.index(num_list[i] + 2)
+            counter += find_ways(start, num_list, max, check_list)
+        elif num_list[i] + 3 in num_list:
+            check_list.append(num_list[i] + 3)
+            print(f'i = {i}, num_list[i] + 3  = {num_list[i] + 3 }, check_list = {check_list}')
+            start = num_list.index(num_list[i] + 3)
+            counter += find_ways(start, num_list, max, check_list)
+        else:
+            break
+    return counter
+
+
+with open('input10_00.txt', 'r') as file_in:
+    num_list = read_file(file_in)
 
 print(num_list)
 num_list = qsort(num_list)
@@ -50,3 +76,6 @@ print(num_list)
 jolt1, jolt3 = find_jolt(num_list)
 mult = jolt1 * jolt3
 print(f'mult = {mult}')
+check_list = []
+counter = find_ways(0, num_list, len(num_list), check_list)
+print(f'counter = {counter}')
